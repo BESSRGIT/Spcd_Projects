1073ae6f528b9e3771204f7898a364647556a4cc
BESSR <45775802+BESSRGIT@users.noreply.github.com>
2019-01-22 10:44:52 +0100

    Modify line for calculating means

--- SAA_Code_20181217.py
+++ SAA_Code_20181217.py
@@ -5,7 +5,7 @@ Created on Tue Dec 18 09:36:39 2018
 "Spiced Academy" Assessment:
 
 A dataset of 20 color-coded (x/y) points is given. The task is to implement a
-k-means clustering algorithm, which divides the points into two separate groups.  
+k-means clustering algorithm, which divides the points into two separate groups.
 
 @author: B. Gür
 """
@@ -28,37 +28,37 @@ combined_set = pd.concat([left_set,right_set.rename(columns={'x.1':'x', 'y.1':'y
 
 
 # Task 1: Calculate means for x and y
-x_mean = combined_set.mean()[0]
-y_mean = combined_set.mean()[1]
-
+#x_mean = combined_set.mean()[0]
+#y_mean = combined_set.mean()[1]
+x_mean, y_mean = combined_set.mean()
 
 # Task 2: Determine red_center and blue_center
 # Separate "combined_set" into two datasets, countaining only red or blue points
 indices_r = combined_set['color'] == 'red'
-Data_Red = combined_set.loc[indices_r, :] 
+Data_Red = combined_set.loc[indices_r, :]
 
 indices_b = combined_set['color'] == 'blue'
-Data_Blue = combined_set.loc[indices_b, :] 
+Data_Blue = combined_set.loc[indices_b, :]
 
 
 # Calculate red_center and blue_center
 red_center_x = Data_Red.mean()[0]
 red_center_y = Data_Red.mean()[1]
-red_center = (red_center_x, red_center_y) 
+red_center = (red_center_x, red_center_y)
 
 blue_center_x = Data_Blue.mean()[0]
 blue_center_y = Data_Blue.mean()[1]
-blue_center = (blue_center_x, blue_center_y) 
+blue_center = (blue_center_x, blue_center_y)
 
 
 # Task 3: Calculate the euclidean distance from each point to blue_center and red_center
 distance_red = []
-distance_blue = []    
+distance_blue = []
 
 for index, row in combined_set.iterrows():
     dist =  sqrt(((red_center_x - combined_set.loc[index][0])**2)   +  (red_center_y - combined_set.loc[index][1])**2)
     distance_red.append(dist)
-    
+
 for index, row in combined_set.iterrows():
     dist =  sqrt(((blue_center_x - combined_set.loc[index][0])**2)   +  (blue_center_y - combined_set.loc[index][1])**2)
     distance_blue.append(dist)
@@ -67,7 +67,7 @@ for index, row in combined_set.iterrows():
 # Task 4: Relabelling points based on their distance to red_center and blue_center
 Diff_red_blue = []
 
-for i in range(len(distance_red)): 
+for i in range(len(distance_red)):
     diff = distance_red[i] - distance_blue[i]
     Diff_red_blue.append(diff)
 
@@ -78,7 +78,7 @@ for i in range(len(Diff_red_blue)):
     else:
         Relabelled.append('blue')
 
-        
+
 # Lists --> Dataframes  (probably not needed)
 distance_red_df = pd.DataFrame(distance_red, columns = ['Distance to red_center'])
 distance_blue_df = pd.DataFrame(distance_blue, columns = ['Distance to blue_enter'])
@@ -98,7 +98,7 @@ indices_nb = New_Dataset['Relabelled Color Code'] == 'blue'
 New_Blue = New_Dataset.loc[indices_nb, :]
 
 
-# Tried to plot for comparison, unfortunately didn't work out :(  Still left the code in though 
+# Tried to plot for comparison, unfortunately didn't work out :(  Still left the code in though
 #plt.subplot(1, 2, 1)
 #plt.scatter(Data_Red['x'], Data_Red['y'], color='r')
 #plt.scatter(Data_Blue['x'], Data_Blue['y'], color='b')
