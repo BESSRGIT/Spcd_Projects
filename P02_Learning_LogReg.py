eec5b96090d87233403088ffe7538b09f1cdc171
BESSR <45775802+BESSRGIT@users.noreply.github.com>
2019-01-22 10:35:47 +0100

    Modification on f-string for identifying sequence

--- P02_Learning_LogReg.py
+++ P02_Learning_LogReg.py
@@ -4,9 +4,9 @@ Created on Mon Jan 14 11:07:58 2019
 
 Spiced Academy Project-02:  With this side project I try to learn a bit more
 about the LogisticRegression function. I would like to know, if and and how
-coefficients change, when parameters are either added or calculated in a 
-different sequence. For simplicity purposes and eventual comparisons the 
-"Titanic" data is being used. 
+coefficients change, when parameters are either added or calculated in a
+different sequence. For simplicity purposes and eventual comparisons the
+"Titanic" data is being used.
 
 @author: DataCoach
 """
@@ -21,9 +21,9 @@ import time
 fn_train = 'train.csv'
 df_train = pd.read_csv(fn_train)
 
-# Pre-Processing: Design training dataframe, with (transformed) features to 
-# be used for modelling. Transformed features are values of type 'str' to 
-# 'int' or 'float' e.g. female --> 1, male --> 0 
+# Pre-Processing: Design training dataframe, with (transformed) features to
+# be used for modelling. Transformed features are values of type 'str' to
+# 'int' or 'float' e.g. female --> 1, male --> 0
 
 # Calculate average age
 average_age = df_train['Age'].dropna().mean()
@@ -41,25 +41,25 @@ list_emb_num = [0,1,2,3]
 df_train['Embarked_num'] = df_train['Embarked'].replace(list_emb, list_emb_num)
 
 # Define y column for regression, i.e. in this case the column 'Survived'
-y_train = df_train['Survived'] 
+y_train = df_train['Survived']
 
 
 # Perform Logistic Regression in a loop with varying starting parameters and
-# varying sequences of the parameters 
+# varying sequences of the parameters
 
 LogReg = LogisticRegression(C=1e5)
 number_of_runs = range(1,11)
 
 for n in number_of_runs:
-    
+
 # Starting Features
     features = ['Pclass', 'Age_complete', 'Gender', 'Embarked_num', 'Fare', 'SibSp', 'Parch']
     shuffle(features)
 
-# Generate empty lists 
+# Generate empty lists
     coef_list  = []
     score_list = []
-    Pclass_coef = []    
+    Pclass_coef = []
     Age_coef = []
     Gender_coef = []
     Embarked_coef = []
@@ -76,7 +76,7 @@ for n in number_of_runs:
     SibSp_index     = features.index('SibSp')
     Parch_index     = features.index('Parch')
 
-# Perform logistic regression 
+# Perform logistic regression
     for i in range(1, len(features) + 1):
         X_matrix = df_train[features[0:i]]
         LogReg.fit(X_matrix, y_train)
@@ -87,8 +87,8 @@ for n in number_of_runs:
         coef.extend(fit_extend)
         coef_list.append(coef)
         score_list.append(score)
- 
-# Generate matrix with all coefficients and scores     
+
+# Generate matrix with all coefficients and scores
     for j in range(0, len(features)):
         Pclass_coef.append(coef_list[j][Pclass_index])
         Age_coef.append(coef_list[j][Age_index])
@@ -100,16 +100,20 @@ for n in number_of_runs:
 
     Coef_Score_Matrix = pd.DataFrame(np.column_stack([Pclass_coef, Age_coef,
                                             Gender_coef, Embarked_coef,
-                                            Fare_coef, SibSp_coef, 
+                                            Fare_coef, SibSp_coef,
                                             Parch_coef, score_list]),
                                columns = ['Pclass', 'Age', 'Gender',
-                                          'Embarked', 'Fare', 'SibSp', 
-                                          'Parch', 'Score'])   
+                                          'Embarked', 'Fare', 'SibSp',
+                                          'Parch', 'Score'])
 
 # Generate string for identification and sequence of parameters
-    f = str()
-    for s in range(0, len(features)):
-        f = f + (str(features[s][0:2]))
+#    f = str()
+#    for s in range(0, len(features)):
+#        f = f + (str(features[s][0:2]))
+
+    f = ''
+    for s in features:
+        f += s[:2]
 
 # Write to excel including timestamp and above mentioned sequence string
     t = time.localtime()
@@ -117,16 +121,7 @@ for n in number_of_runs:
     Coef_Score_Matrix.to_excel(str(timestamp) + '_Coefficients_' + f + '.xlsx')
 
 
-# Data analysis and visualization 
+# Data analysis and visualization
 ######
 #TO BE DONE
 ######
-
- 
-
-
-
-
-
-
-    
